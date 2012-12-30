e7cce767c58b1ced5989f3563d4e428f39b6c050
infoslug <dudeman@server.fake>
2012-12-30 14:05:18 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '6b802edd923359e2dc403f95c7d3b7970d4531afa217888c7b4dc67a022c19cd5c2f4c946b781f09b09fad2b8241d156489e2cb370bceb5f76868f90525eba85'
